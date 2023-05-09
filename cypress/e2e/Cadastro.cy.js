062c6b3fc35cf7f2d015574c30b634bd2d1bb545
Jackson <jk-jackson@hotmail.com>
2023-05-09 19:05:24 -0300

    Fazendo uso dos comandos personalizados

--- cypress/e2e/Cadastro.cy.js
+++ cypress/e2e/Cadastro.cy.js
@@ -7,56 +7,30 @@ describe('Executando testes na tela de cadastro', () => {
   })
 
   it('Verifica mensagem validação dos campos', () => {
-    cy.contains('a', 'Register now').click()
-    cy.contains('button', 'Register').click()
-
-    cy.contains('ap-vmessage', 'Email is required!').should('be.visible')
-    cy.contains('button', 'Register').click()
-
-    cy.contains('ap-vmessage', 'Full name is required!').should('be.visible')
-    cy.contains('ap-vmessage', 'User name is required!').should('be.visible')
-    cy.contains('ap-vmessage', 'Password is required!').should('be.visible')
-
-    cy.get('ap-vmessage').should('have.length', 4)
+    cy.VerificaMensagemValidacao()
 
   })
 
   it('Verifica mensagem de e-mail inválido', () => {
-    cy.contains('a', 'Register now').click()
-
-    cy.get('input[placeholder="email"]').type('jack.com')
-    cy.contains('button', 'Register').click()
-    cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible')
+    cy.MensagemEmailInvalido()
 
   })
 
   it('Verifica mensagem de senha com menos de 8 caracteres', () => {
-    cy.contains('a', 'Register now').click()
-
-    cy.get('input[placeholder="password"]').type('jk123')
-    cy.contains('button', 'Register').click()
-    cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible')
+    cy.VerificaMensagemSenha()
 
   })
 
   it('Cadastro de um novo usário', () => {
-    cy.contains('a', 'Register now').click()
-
-    cy.get('input[placeholder="email"]').type('jacks1@gmail.com')
-    cy.get('input[placeholder="full name""]').type('jack gonca')
-    cy.get('input[placeholder="user name""]').type('jack1234')
-    cy.get('input[placeholder="password"]').type('12345678')
-
-    // cy.contains('button', 'Register').click()
+    cy.CadastroUsuario()
 
   })
 
   const usuarios = require('../fixtures/usuarios.json')
   usuarios.forEach((usuario) => {
-    it.only(`Criando vários usuários ${usuario.userName}`, () => {
-
+    it(`Criando vários usuários ${usuario.userName}`, () => {
+      // cy.CadastroVariosUsuario()
       cy.contains('a', 'Register now').click()
-
       cy.get('input[placeholder="email"]').type(usuario.email)
       cy.get('input[placeholder="full name"]').type(usuario.fullname)
       cy.get('input[placeholder="user name"]').type(usuario.userName)
@@ -67,6 +41,5 @@ describe('Executando testes na tela de cadastro', () => {
     })
 
   })
-
 
 })
--- cypress/support/gui_login_cadastro.js
+++ cypress/support/gui_login_cadastro.js
@@ -1,3 +1,7 @@
+
+
+//  *********** COMANDOS PERSONALIZADOS DE LOGIN ***********
+
 Cypress.Commands.add('loginValido', (nome,senha) =>{
     cy.get('input[placeholder="user name"]').type(nome)
     cy.get('input[placeholder="password"]').type(senha, {log: false})
@@ -12,3 +16,58 @@ Cypress.Commands.add('loginInvalido', (nome,senha) =>{
 
     cy.contains('button', 'login').click()
 })
+
+
+//  ********  COMANDO PERSONALIZADOS DE CADASTRO ***********
+
+Cypress.Commands.add('VerificaMensagemValidacao', ()=>{
+
+    cy.contains('a', 'Register now').click()
+    cy.contains('button', 'Register').click()
+
+    cy.contains('ap-vmessage', 'Email is required!').should('be.visible')
+    cy.contains('button', 'Register').click()
+
+    cy.contains('ap-vmessage', 'Full name is required!').should('be.visible')
+    cy.contains('ap-vmessage', 'User name is required!').should('be.visible')
+    cy.contains('ap-vmessage', 'Password is required!').should('be.visible')
+    cy.get('ap-vmessage').should('have.length', 4)
+})
+
+Cypress.Commands.add('MensagemEmailInvalido', ()=>{
+
+    cy.contains('a', 'Register now').click()
+    cy.get('input[placeholder="email"]').type('jack.com')
+    cy.contains('button', 'Register').click()
+    cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible')
+})
+
+Cypress.Commands.add('VerificaMensagemSenha', ()=>{
+
+    cy.contains('a', 'Register now').click()
+    cy.get('input[placeholder="password"]').type('jk123')
+    cy.contains('button', 'Register').click()
+    cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible')
+})
+
+Cypress.Commands.add('CadastroUsuario', ()=>{
+
+    cy.contains('a', 'Register now').click()
+    cy.get('input[placeholder="email"]').type('jacks1@gmail.com')
+    cy.get('input[placeholder="full name"]').type('jack gonca')
+    cy.get('input[placeholder="user name"]').type('jack1234')
+    cy.get('input[placeholder="password"]').type('12345678')
+
+    // cy.contains('button', 'Register').click()
+})
+
+// Cypress.Commands.add('CadastroVariosUsuario', ()=>{
+
+//     cy.contains('a', 'Register now').click()
+//     cy.get('input[placeholder="email"]').type(usuario.email)
+//     cy.get('input[placeholder="full name"]').type(usuario.fullname)
+//     cy.get('input[placeholder="user name"]').type(usuario.userName)
+//     cy.get('input[placeholder="password"]').type(usuario.passowd)
+
+//     // cy.contains('button', 'Register').click()
+// })
